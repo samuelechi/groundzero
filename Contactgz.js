42c6b022dabd8121fc3a42bf43894f8bd8c00c11
samuelechi <edomebube@yahoo.com>
2025-12-17 12:12:56 -0600

    Delete Contactgz.js

--- Contactgz.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuPlaceholder = document.getElementById('contact-bar');
-    fetch('Contact us.html')
-        .then(response => response.text())
-        .then(data => {
-            menuPlaceholder.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading menu:', error));
-});
